ca44ef221f757cd001d89a66c0c2eda5248ae38a
karenmahr <karenmahrh@gmail.com>
2025-05-06 21:58:58 -0300

    Assignment week 01

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int grades = int.Parse(grade);
+        string letter= "";
+        string sign = "";
+        
+        if (grades >=90)
+        {
+            letter= "A";
+        }
+        else if (grades >= 80)
+        {
+            letter= "B";
+        }
+        else if (grades >=70)
+        {
+            letter= "C";
+        }
+        else if (grades >=60)
+        {
+            letter= "D";
+        }
+        else
+        {
+            letter= "F";
+        }
+
+        int lastDigit = grades % 10;
+   
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit <= 3)
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = " ";
+        }
+
+        if (grades >= 94 || grades < 60)
+        {
+            sign = " ";
+        }
+
+        Console.WriteLine($"Your grade is an {letter}{sign}");
+
+
+        if (grades >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the course. Keep on trying");
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished. ");   
+
+        List<int> numberList = new List<int>();
+        int number; 
+
+        do 
+        {
+            Console.Write("Enter a number: "); 
+            string numbers = Console.ReadLine();
+            number = int.Parse(numbers);  
+
+            if (number != 0)
+            {
+                numberList.Add(number);
+            }
+        } while (number != 0) ;
+
+            int sum =numberList.Sum();
+            double average = numberList.Average();
+            int max =numberList.Max();
+            int min = numberList.Where(n => n > 0).Min();
+            numberList.Sort();
+            
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {max}");
+            Console.WriteLine($"The smallest positive number is: {min}");
+            Console.WriteLine($"The sorted list is: ");
+            foreach (int element in numberList)
+            {
+                Console.WriteLine(element);
+            }
     }
 }
