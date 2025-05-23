632ec8cb4d689d8de3ae027eacb01eff9bb45e14
karenmahr <karenmahrh@gmail.com>
2025-05-23 20:14:43 -0300

    Deleted an unnecesary for loop

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -4,42 +4,46 @@ public class Scripture
     private Reference _reference;
     private List<Word> _words = new List<Word>();
 
-    public Scripture(Reference Reference, string text)
+    public Scripture(Reference reference, string text)
     {
-        _reference = Reference;
+        _reference = reference;
         _words = new List<Word>();
 
         string[] wordSplit = text.Split(' ');
-        
-        foreach(string word in wordSplit)
+
+        foreach (string word in wordSplit)
         {
             Word newWord = new Word(word);
             _words.Add(newWord);
         }
+    }
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
 
+        int index = random.Next(_words.Count);
+        _words[index].Hide();
+        
     }
-    private void HideRandomWords(int numberToHide)
+    public string GetDisplayText()
     {
-       Random randomGenerator = new Random();
-        int numberToHide = randomGenerator.Next(1, 3);
-    }
-
-    private string GetDisplayText()
-    {
-        foreach (Word Word in _words)
+        string fullText = "";
+        foreach (Word word in _words)
         {
-        if (Word = Show())
-        {
-            return _text;
-        }  
-        else
-        {
-            return "_____";
+            fullText += word.GetDisplayText() + " ";
         }
-        }
+        return $"{_reference.GetDisplayText()} {fullText}";
     }
-    private bool IsCompletelyHidden()
+    public bool IsCompletelyHidden()
     {
-     
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
+
