86f4c464a5b3a0c01ab371e350ed292580a235be
karenmahr <karenmahrh@gmail.com>
2025-05-23 20:12:10 -0300

    Edited the while loop, and organized the code so it can be displayed as it should

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        Reference r1 = new Reference("Proverbs", 3, 5, 6);
+        Scripture s1 = new Scripture(r1, "Trust in the Lord with all thine heart and lean not unto thine own understanding.");
+
+
+        Console.WriteLine(s1.GetDisplayText());
+
+        while (!s1.IsCompletelyHidden())
+        {
+            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+            string input = Console.ReadLine();
+            Console.Clear();
+            
+            if (input == "quit")
+            {
+                break;
+            }
+
+            s1.HideRandomWords(1);
+
+            Console.WriteLine(s1.GetDisplayText());
+        }
     }
 }
