e1c3cb484b28908390037b41737a1c5874123824
karenmahr <karenmahrh@gmail.com>
2025-05-27 20:04:59 -0300

    Classes, attributes and constructors created

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Comment
+{
+    private string _title;
+    private string _author;
+    private string _lenght;
+
+    public Comment(string title, string author, string lenght)
+    {
+        _title = title;
+        _author = author;
+        _lenght = lenght;
+    } 
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Video
+{
+    private string _name;
+    private string _text;
+
+    public Video(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+}
