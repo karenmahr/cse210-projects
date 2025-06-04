5676fc1f58845e8fc5309872c05ebfc2b302adad
karenmahr <karenmahrh@gmail.com>
2025-06-03 22:54:46 -0300

    Wrote main program and worked more on the activity class

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -15,17 +15,44 @@ class Activity
     public void DisplayStartingMessage()
     {
         Console.WriteLine($"Welcome to the {_name} \n {_description} \n How long, in seconds, would you like for your session?");
+        string userInput = Console.ReadLine();
+        int seconds = int.Parse(userInput);        
     }
     public void DisplayEndingMessage()
     {
         Console.WriteLine($"Well done!!\n You have completed another {_duration} seconds of the {_name}");
     }
+    
+    //NOT QUITE THERE, BUT VERY CLOSE
     public void ShowSpinner(int seconds)
     {
+        List<string> animationStrings = new List<string>();
+        foreach (string animation in animationStrings)
+        {
+            animationStrings.Add("|");
+            animationStrings.Add("/");
+            animationStrings.Add("-");
+            animationStrings.Add("\\");
+            animationStrings.Add("|");
+            animationStrings.Add("/");
+            animationStrings.Add("-");
+            animationStrings.Add("\\");
+
+            Console.WriteLine($"Get ready... \n {animation}");
+        }
 
     }
     public void ShowCountDown(int seconds)
     {
-        
+        DateTime startTime = DateTime.Now;
+        DateTime futureTime = startTime.AddSeconds(seconds);
+
+        Thread.Sleep(seconds);
+
+        DateTime currentTime = DateTime.Now;
+        if (currentTime < futureTime)
+        {
+            Console.WriteLine("We have not arrived at our future time yet...");
+        }
     }
 }
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,5 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
+        Activity a1 = new Activity("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing", 30);
+        ReflectingActivity r1 = new ReflectingActivity("Reflection Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.", 40);
+        ListingActivity l1 = new ListingActivity("Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.", 50, 0);
+
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("   1. Start breathing activity");
+            Console.WriteLine("   2. Start reflecting activity");
+            Console.WriteLine("   3. Start listing activity");
+            Console.WriteLine("   4. Quit");
+            Console.Write("Select a choice from the menu:");
+            string userInput = Console.ReadLine();
+            int option = int.Parse(userInput);
+
+            if (option == 1)
+            {
+                a1.DisplayStartingMessage();
+                Console.WriteLine("Breathe in");
+                Console.WriteLine("Breathe out");                
+                a1.DisplayEndingMessage();
+            }
+            else if (option == 2)
+            {
+                r1.DisplayStartingMessage();
+
+                string prompt = r1.GetRandomPrompt();
+                Console.WriteLine(prompt);
+
+                string question = r1.GetRandomQuestions();
+                Console.WriteLine(question);
+
+                r1.DisplayEndingMessage();
+            }
+            else if (option == 3)
+            {
+                l1.DisplayStartingMessage();
+                l1.GetRandomPrompt();
+                l1.DisplayEndingMessage();
+            }
+            else if (option == 4)
+            {
+                Console.WriteLine("Goodbye");
+                running = false;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid option");
+            }
+        }
     }
 }
